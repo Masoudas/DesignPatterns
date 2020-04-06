02b9473de3abef2ba5ee2bdc41d5359dcded1c30
Masoudas <Masoudas@hotmai.com>
2020-04-06 08:45:20 +0200

    Add flyweight

---
+++ src/main/java/GoF/Structural/FlyWieght/1_Basics.java
@@ -0,0 +1,9 @@
+/**
+ * For example in a text editor, it's best to define a char object for each
+ * particular character, which allows flexibility of design. The problem however
+ * is that making several thousand characters for the document is costly in terms of memory.
+ * FlyWeight allows object sharing among objects.
+ * 
+ * See https://www.tutorialspoint.com/design_pattern/flyweight_pattern.htm and
+ * https://refactoring.guru/design-patterns/flyweight/java/example.
+ */
