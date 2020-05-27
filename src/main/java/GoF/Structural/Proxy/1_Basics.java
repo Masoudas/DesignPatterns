f7157cf783a4b7f09f38f58766f8fd6bcf55f68f
Masoudas <Masoudas@hotmai.com>
2020-05-27 07:27:23 +0200

    Start proxy pattern

---
+++ src/main/java/GoF/Structural/Proxy/1_Basics.java
@@ -0,0 +1,14 @@
+package GoF.Structural.Proxy;
+
+/**
+ * 
+ * 
+ * Related patterns:
+ * 
+ * (Me: The proxy pattern is essentially like the decorator pattern (in most
+ * cases), in that it (usually) implements the same interface as the object it
+ * proxies. The difference however is that proxy does not add new behavior,
+ * whereas decorator does. Moreover, the pattern resembles adaptor in the sense
+ * that it adapts the object for some needs, the difference however is that
+ * adaptor changes the interface for a completely new application.)
+ */
